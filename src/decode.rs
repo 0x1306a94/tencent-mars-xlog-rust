2e75f00414d82cb9f6eb233cad9649734b860550
0x1306a94 <0x1306a94@gmail.com>
2022-01-09 16:32:14 +0800

    WIP

--- src/decode.rs
+++ src/decode.rs
@@ -1,11 +1,12 @@
-use anyhow::Result;
+use anyhow::{Error, Result};
 use memmap::Mmap;
 use std::convert::TryInto;
 use std::error;
 use std::fmt;
 use std::fs::File;
 use std::fs::OpenOptions;
-use std::io::{Error, Write};
+use std::io;
+use std::io::Write;
 use std::path::PathBuf;
 
 pub trait ReadInteger<T> {
@@ -26,7 +27,7 @@ macro_rules! impl_read_integer {
     }
 }
 
-impl_read_integer!(u8, i16, i32, u32, i64);
+impl_read_integer!(u8, i16, u16, i32, u32, i64);
 
 fn read_integer<T: ReadInteger<T>>(data: &[u8]) -> T {
     T::from_le_bytes(&data[..std::mem::size_of::<T>()])
@@ -51,11 +52,13 @@ mod magic {
     pub const END: u8 = 0x00;
 }
 
+const BASE_KEY: u8 = 0xcc;
+
 pub struct Context {
     input: String,
     output: String,
     encrypted: bool,
-    last_seq: i8,
+    last_seq: i16,
 }
 
 struct InputBuffer {
@@ -100,6 +103,9 @@ impl InputBuffer {
     }
 
     fn read_range_bytes(&self, pos: usize, len: usize) -> &[u8] {
+        if (pos + len) > self.file_len {
+            println!("越界");
+        }
         &self.mmap[pos..pos + len]
     }
 }
@@ -113,9 +119,11 @@ impl OutputBufFile {
     fn new(path: &str) -> anyhow::Result<OutputBufFile> {
         let out_file = OpenOptions::new()
             .write(true)
-            .read(true)
+            // .read(true)
             .create(true)
             .open(&path)?;
+
+        // out_file.write_all([0x0]);
 
         let buf = OutputBufFile {
             file: out_file,
@@ -142,7 +150,8 @@ impl Context {
     ) -> anyhow::Result<usize> {
         let in_buf_len = input_buf_file.len();
         if *offset >= in_buf_len {
-            return Err(anyhow::anyhow!("超出文件大小"));
+            let eof = io::Error::new(io::ErrorKind::UnexpectedEof, "failed to fill whole buffer");
+            return Err(anyhow::Error::new(eof));
         }
         let in_buf = input_buf_file.bytes();
         if !is_good_log_buf(in_buf, in_buf_len, *offset, 1) {
@@ -163,8 +172,6 @@ impl Context {
         }
 
         let mut crypt_key_len: usize = 0;
-        let mut header_len: usize = 0;
-
         let value = input_buf_file.bytes()[0];
         if magic::NO_COMPRESS_START == value
             || magic::COMPRESS_START == value
@@ -183,13 +190,65 @@ impl Context {
             crypt_key_len = 64
         } else {
             output_buf_file.appen_str(&format!(
-                "in DecodeBuffer _buffer[{:?}]:{:?} != MAGIC_NUM_STARTn",
+                "in DecodeBuffer _buffer[{:?}]:{:?} != NUM_START\n",
                 *offset, value
             ));
             return Err(anyhow::anyhow!("无法获取 log start pos"));
         }
 
-        Ok(0)
+        let header_len = 1 + 2 + 1 + 1 + 4 + crypt_key_len;
+        let start = *offset + header_len - crypt_key_len - 4;
+        let len_bytes = input_buf_file.read_range_bytes(start, 4);
+        let length = read_integer::<u32>(len_bytes) as usize;
+
+        let pos = *offset + header_len - crypt_key_len - 4 - 2 - 2;
+        let sqe_bytes = input_buf_file.read_range_bytes(pos, 2);
+        let seq = read_integer::<i16>(sqe_bytes);
+        let pos = *offset + header_len - crypt_key_len - 4 - 1 - 1;
+        let begin_hour = input_buf_file.read_range_bytes(pos, 1)[0];
+        let pos = *offset + header_len - crypt_key_len - 4 - 1;
+        let end_hour = input_buf_file.read_range_bytes(pos, 1)[0];
+        let v = input_buf_file.all_bytes()[*offset] as i32;
+        let key = ((BASE_KEY as i32) ^ (0xff & (seq as i32)) ^ v);
+
+        if seq != 0 && seq != 1 && self.last_seq != 0 && seq != (self.last_seq + 1) {
+            output_buf_file.appen_str(&format!(
+                "[F]decode_log_file.py log seq:{:?}-{:?} is missing\n",
+                self.last_seq + 1,
+                seq - 1
+            ));
+        }
+
+        if seq != 0 {
+            self.last_seq = seq;
+        }
+
+        let pos = *offset + header_len;
+        let len = length;
+        let temp_bytes = input_buf_file.read_range_bytes(pos, len);
+        let value = input_buf_file.all_bytes()[*offset];
+
+        if self.encrypted {
+            if magic::NO_COMPRESS_START1 == value || magic::SYNC_ZSTD_START == value {
+                // pass
+                output_buf_file.appen_str("pass\n");
+            } else if magic::COMPRESS_START2 == value || magic::ASYNC_ZSTD_START == value {
+                // 加密
+            } else if magic::ASYNC_NO_CRYPT_ZSTD_START == value {
+            }
+        } else {
+            if magic::NO_COMPRESS_START1 == value
+                || magic::COMPRESS_START2 == value
+                || magic::SYNC_ZSTD_START == value
+                || magic::ASYNC_ZSTD_START == value
+            {
+                println!("use wrong decode script");
+                output_buf_file.appen_str("use wrong decode script\n");
+            }
+        }
+        // TODO: 暂时中断
+        output_buf_file.appen_str(&format!("pos: {:?}\n", *offset + header_len + length + 1));
+        return Ok(*offset + header_len + length + 1);
     }
 }
 
@@ -218,11 +277,15 @@ impl Context {
         let mut output_buf_file = OutputBufFile::new(&self.output)?;
         loop {
             match self.decode_buf(&mut input_buf_file, &mut start_pos, &mut output_buf_file) {
-                Ok(pos) => start_pos = pos,
-                Err(e) => return Err(e),
+                Ok(pos) => {
+                    start_pos = pos;
+                    println!("start_pos: {:?}", start_pos);
+                }
+                Err(e) => {
+                    return Err(e);
+                }
             }
         }
-        Ok(())
     }
 }
 
@@ -322,7 +385,16 @@ mod tests {
         let mut ctx = Context::new(input, output, encrypted);
         match ctx.decode() {
             Ok(_) => println!("成功"),
-            Err(e) => assert!(false, "err||: {:?}", e),
+            Err(e) => {
+                for cause in e.chain() {
+                    if let Some(io_error) = cause.downcast_ref::<io::Error>() {
+                        if io_error.kind() == io::ErrorKind::UnexpectedEof {
+                            return;
+                        }
+                    }
+                }
+                assert!(false, "{:?}", e.root_cause());
+            }
         }
     }
 }
