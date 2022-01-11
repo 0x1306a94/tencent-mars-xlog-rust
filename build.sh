269f5975e0bbeae5b1dae7e3214fc9278193ac80
0x1306a94 <0x1306a94@gmail.com>
2022-01-11 10:31:17 +0800

    add build.sh

---
+++ build.sh
@@ -0,0 +1,31 @@
+#!/bin/bash 
+
+set -e
+
+MAC_X86_TARGET=x86_64-apple-darwin
+MAC_ARM64_TARGET=aarch64-apple-darwin
+OUT_NAME=tencent-mars-xlog-util
+
+function useage() {
+    echo "Useage:"
+    echo ""
+    echo "  ./build.sh --out dir"
+    exit 0
+}
+
+if [ ! -n "$1" ]; then
+    useage
+elif [ ! -n "$2" ]; then
+    useage
+elif [ "$1" == "--out" ]; then
+
+    rustup target add $MAC_X86_TARGET
+    rustup target add $MAC_ARM64_TARGET
+
+    cargo build --release --target=$MAC_X86_TARGET
+    cargo build --release --target=$MAC_ARM64_TARGET
+
+    mkdir -p $2
+    lipo -create -output $2/$OUT_NAME ./target/$MAC_X86_TARGET/release/$OUT_NAME ./target/$MAC_ARM64_TARGET/release/$OUT_NAME
+fi
+
